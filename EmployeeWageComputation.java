ed02c511b7e43eaa1fc7ada6ef354be080eb3d0e
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 10:29:57 +0530

    Added : Part & Full Time Employee Wage Program

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,20 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation ");
+
+        int EMP_IS_FULL_TIME = 1, EMP_WAGE_PER_HOUR = 20, EMP_IS_PART_TIME = 2,
+                empWage = 0, empWorkingHrs=0;
+
+        int empCheck = (int)((Math.random() * 10) % 3);
+
+        if(empCheck == EMP_IS_FULL_TIME){
+            empWorkingHrs = 8;
+        } else if (empCheck == EMP_IS_PART_TIME) {
+            empWorkingHrs = 4;
+        } else{
+            empWorkingHrs = 0;
+        }
+        empWage = EMP_WAGE_PER_HOUR * empWorkingHrs;
+        System.out.println("Daily Employee Wage : " + empWage);
     }
 }
