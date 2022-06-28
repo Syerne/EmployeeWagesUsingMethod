90e4d45f56342387c00f6a9a806420ca5c230972
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 10:56:01 +0530

    Added : Employee Wage Per Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,29 @@
 public class EmployeeWageComputation {
+
+    static final int EMP_IS_FULL_TIME = 1;
+    static final int EMP_IS_PART_TIME = 2;
+    static final int EMP_WAGE_PER_HOUR = 20;
+    static final int EMP_MAX_WORKING_DAYS = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation ");
+
+               int empWage = 0, empWorkingHrs = 0;
+
+        int empCheck = (int) ((Math.random() * 10) % 3);
+
+        switch (empCheck) {
+            case 1:
+                empWorkingHrs = 8;
+                break;
+            case 2:
+                empWorkingHrs = 4;
+                break;
+            default:
+                empWorkingHrs = 0;
+                break;
+        }
+        empWage = EMP_WAGE_PER_HOUR * empWorkingHrs * EMP_MAX_WORKING_DAYS;
+        System.out.println("Daily Employee Wage : " + empWage);
     }
 }
